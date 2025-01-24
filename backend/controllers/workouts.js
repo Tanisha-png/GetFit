4fa26860f5c791e8ed5eff348ff67143b6b631a0
Tanisha-png <user@email.com>
2025-01-24 15:25:55 -0500

    Create workout controller

---
+++ backend/controllers/workouts.js
@@ -0,0 +1,8 @@
+const Workout = require('../models/workout');
+
+module.exports = {
+    create,
+    index
+};
+
+// GET /api/workouts (INDEX action)
---
+++ backend/routes/workouts.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const workoutsCtrl = require('../controllers/workouts');
+
+// All path start with '/api/workouts'
+
