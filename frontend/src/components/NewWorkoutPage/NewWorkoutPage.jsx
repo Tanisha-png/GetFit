40aeba71d1efb232c20114882973455c5df8783a
Tanisha-png <user@email.com>
2025-01-27 11:28:51 -0500

    Add handleChange to NewWorkoutPage

--- frontend/src/components/NewWorkoutPage/NewWorkoutPage.jsx
+++ frontend/src/components/NewWorkoutPage/NewWorkoutPage.jsx
@@ -1,11 +1,29 @@
 import React,  { useState } from 'react';
 import { useNavigate } from 'react-router';
 
-const Workout = () => {
-    const [workouts, setWorkouts] = useState([]);
+const initialState = {
+    sets: '',
+    reps: '',
+    weight: '',
+    day: Date,
+    exercises: [],
+};
+
+const NewWorkoutPage = ({addWorkout}) => {
+    const [workout, setWorkout] = useState(initialState);
+
+    const navigate = useNavigate();
+
+    const handleChange = ({target}) => {
+        setWorkout({...workout, [target.name]: target.value });
+    };
+
+    const handleSubmit =
 
 
 
 
     
 }
+
+export default NewWorkoutPage;
