649f67724aa6b7d0f490bccadf150d7bd0a3cc91
Tanisha-png <user@email.com>
2025-01-31 08:30:25 -0500

    Update code in posts controllers

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -6,15 +6,17 @@ module.exports = {
 };
 
 async function index(req, res) {
-  const posts = await Post.find({}).populate('user').sort('-createdAt');
+  // const posts = await Post.find({}).populate('user').sort('-createdAt');
+  const posts = await Post.find({});
   res.json(posts);
 }
 
 async function create(req, res) {
   try {
     req.body.user = req.user._id;
-    const post = await Post.create(req.body);
-    res.json(post);
+    // const post = await Post.create(req.body);
+    const posts = await Post.find({user: req.user._id})
+    res.json(posts);
   } catch (err) {
     console.log(err);
     res.status(400).json({ message: 'Create Post Failed' });
